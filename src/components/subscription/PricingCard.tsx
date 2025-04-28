a6b433829c5bc141cac224c7eb27ebb9fec5ecc9
Saem's Tunes <saemstunes@gmail.com>
2025-04-28 22:59:12 +0300

    Update PricingCard.tsx
    Added Variant and Classname as string elements so that PricingCard.tsx can get rid of their error

--- src/components/subscription/PricingCard.tsx
+++ src/components/subscription/PricingCard.tsx
@@ -9,6 +9,8 @@ import { useToast } from "@/hooks/use-toast";
 
 interface PricingCardProps {
   plan: SubscriptionPlan;
+  variant?: string;
+  className?: string;
 }
 
 const PricingCard = ({ plan }: PricingCardProps) => {
