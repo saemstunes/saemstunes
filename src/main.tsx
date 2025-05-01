072c7b7deaf86a0b64a927f15600b1909b1581f1
Civic Education Kenya <civiceducationkenya@gmail.com>
2025-05-01 12:00:15 +0300

    Update main.tsx
    Final edits

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+// src/main.tsx
+import './styles/global.css';
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './styles/global.css'; // src/main.tsx
 
 createRoot(document.getElementById("root")!).render(<App />);
