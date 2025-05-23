09681851a35cc02ba820bf02556373714157ca00
Saem's Tunes <saemstunes@gmail.com>
2025-05-23 18:13:56 +0300

    Update VideoDetail.tsx
    Syntax error from subscription to subscriptions

--- src/pages/VideoDetail.tsx
+++ src/pages/VideoDetail.tsx
@@ -169,7 +169,7 @@ const VideoDetail = () => {
             This video is only available to premium subscribers. Upgrade your account to access all content.
           </p>
           <Button 
-            onClick={() => navigate("/subscription")}
+            onClick={() => navigate("/subscriptions")}
             className="mt-6 bg-gold hover:bg-gold-dark text-white"
           >
             Upgrade Now
