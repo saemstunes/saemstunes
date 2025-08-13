66641b3ab5d8ea224771bf10f246d21521ef1fd6
Saem's Tunes <saemstunes@gmail.com>
2025-08-13 22:24:12 +0300

    Update main.tsx
    Signed-off-by: Saem's Tunes <saemstunes@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 // src/main.tsx
 import './styles/global.css';
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+// Import and register GSAP plugins globally
+import { gsap } from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+
+// Register GSAP plugins
+gsap.registerPlugin(SplitText);
+
+// Set GSAP global defaults
+gsap.defaults({
+  ease: "power3.out",
+  duration: 0.5
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
