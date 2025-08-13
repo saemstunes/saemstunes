352219cd20b69edd63914c9870ef7c6aefc1e828
Saem's Tunes <saemstunes@gmail.com>
2025-08-13 22:24:34 +0300

    Update gsap.d.ts
    Signed-off-by: Saem's Tunes <saemstunes@gmail.com>

--- src/types/gsap.d.ts
+++ src/types/gsap.d.ts
@@ -3,3 +3,8 @@ declare module "gsap/SplitText" {
   export * from "gsap/SplitText";
   export { SplitText as default } from "gsap/SplitText";
 }
+
+declare module "gsap/all" {
+  export * from "gsap/all";
+  export { gsap as default } from "gsap";
+}
